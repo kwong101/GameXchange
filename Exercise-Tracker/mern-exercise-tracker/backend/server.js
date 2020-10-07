16dc61721ac49991ee90c17aa49b9feb4fd94ad5
Troy Knatt <troyknatt@gmail.com>
2020-10-06 22:28:08 -0700

    had nested .git files

---
+++ Exercise-Tracker/mern-exercise-tracker/backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//uri is our db uri, where db is stored
+//we get uri from mongodb dashboard
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } );
+
+const connection = mongoose.connection;
+// once it's connected, log the text 
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
