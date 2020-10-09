1415eb1337a023fd6476a931969f84426614a1cd
Troy Knatt <troyknatt@gmail.com>
2020-10-09 05:08:53 -0700

    redid navbar, made it responsive, and added search bar, and renamed to GameXchange

--- Exercise-Tracker/mern-exercise-tracker/src/components/navbar.component.js
+++ Exercise-Tracker/mern-exercise-tracker/src/components/navbar.component.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {Navbar, Form, FormControl, Button} from 'react-bootstrap'
 
-export default class Navbar extends Component {
+
+const logo = require('../ge_logo.svg');
+
+export default class MyNavbar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(){
+    this.setState({ menu: !this.state.menu})
+  }
 
   render() {
+
+    const show = (this.state.menu) ? "show" : "" ;
+
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              alt="GameXchange Logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            GameXchange
+          </Navbar.Brand>
+        
+        </Navbar>
+
+        {/* 
         <Link to="/" className="navbar-brand">GameXchange</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-        </ul>
+        */}
+
+        <button className="navbar-toggler" type="button" onClick={ this.toggleMenu }>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={"collapse navbar-collapse " + show}>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Exercises</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Exercise Log</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+            </li>
+          </ul>
+
+
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+
+
         </div>
+
       </nav>
-    );
+    
+      );
   }
 }
